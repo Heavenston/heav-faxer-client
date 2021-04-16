1098412df0573b268b275f9e4ec2f70ccd852f05
Heavenstone <malolegendrelemaire@gmail.com>
2021-04-16 10:53:20 +0200

    BUTTON

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,31 @@
 <script lang="ts">
+    import Button from "./components/Button.svelte";
+    let isReleased = true;
 </script>
 
 <div
     class="absolute inset-0 flex justify-center items-center bg-gray-800 text-white"
 >
     <div class="px-10 py-7 min-w-96 bg-gray-700 shadow-lg rounded">
-        <div class="relative w-full rounded-full h-3 bg-gray-900">
-            <div
-                class="absolute h-40 bg-white inset-x-2 bottom-1.5 rounded-t-sm hover:h-10 transition-all duration-200"
-            />
+        <div>
+            <div class="relative w-full rounded-full h-3 bg-gray-900">
+                <div class="absolute h-40 inset-x-2 bottom-1.5 overflow-hidden">
+                    <div
+                        class="h-40 bg-white rounded-t-sm transition-all duration-1000 ease-linear transform {isReleased
+                            ? 'translate-y-20'
+                            : ''}"
+                    />
+                </div>
+            </div>
+        </div>
+        <div class="pt-10">
+            <Button
+                on:click={() => {
+                    isReleased = !isReleased;
+                }}
+            >
+                Hi
+            </Button>
         </div>
     </div>
 </div>
---
+++ src/components/Button.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    let isHover = false;
+    let isPress = false;
+
+    const hoverFn = (v: boolean) => () => {
+        isHover = v;
+    };
+    const pressFn = (v: boolean) => () => {
+        isPress = v;
+    };
+
+    let translateSize = "2";
+    $: translateSize = isHover ? (isPress && isHover ? "0" : "0.5") : "1";
+</script>
+
+<button
+    class="bg-gray-900 rounded focus:outline-none shadow-sm"
+    on:click
+    on:mouseover={() => {
+        isHover = true;
+    }}
+    on:mouseout={() => {
+        isHover = false;
+    }}
+    on:mousedown={() => {
+        isPress = true;
+    }}
+    on:mouseup={() => {
+        isPress = false;
+    }}
+>
+    <div
+        class="px-4 py-1 transform -translate-y-{translateSize} bg-gray-800 rounded"
+    >
+        <slot />
+    </div>
+</button>
