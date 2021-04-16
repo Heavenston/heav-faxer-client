884487988701363e8aed89384bb23aec0bbf87e0
Heavenstone <malolegendrelemaire@gmail.com>
2021-04-16 10:17:40 +0200

    Paper

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,14 @@
 <script lang="ts">
 </script>
 
-<h1>Hello Svelte!</h1>
+<div
+    class="absolute inset-0 flex justify-center items-center bg-gray-800 text-white"
+>
+    <div class="px-10 py-7 min-w-96 bg-gray-700 shadow-lg rounded">
+        <div class="relative w-full rounded-full h-3 bg-gray-900">
+            <div
+                class="absolute h-40 bg-white inset-x-2 bottom-1.5 rounded-t-sm hover:h-10 transition-all duration-200"
+            />
+        </div>
+    </div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
     purge: ["src/**/*.{svelte,html}"],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                gray: colors.trueGray,
+            },
+            minWidth: t => t("spacing"),
+        },
     },
     variants: {
-        extend: {},
+        extend: {
+            height: ["hover"],
+        },
     },
     plugins: [],
 };
