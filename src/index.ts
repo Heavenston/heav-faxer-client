9b00a00f171c461e5cc71c6828af733d8e3412bf
Heavenstone <malolegendrelemaire@gmail.com>
2021-04-24 20:08:17 +0200

    File download !

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,39 @@
 import Index from "./pages/Index.svelte";
+import type { SvelteComponent } from "svelte";
 
-if (location.pathname === "/") {
-    new Index({
-        target: document.getElementById("root") as HTMLElement,
-    });
-} else {
-    location.replace("/");
+const pages = {
+    nothing: () => null,
+    index: () =>
+        new Index({
+            target: document.getElementById("root") as HTMLElement,
+        }),
+    download: () => {
+        location.href = "http://files.faxer.heav.fr/" + location.hash.substr(1);
+        return null;
+    },
+};
+
+let currentElement: SvelteComponent | null = null;
+let currentPageName = "";
+function render() {
+    let nextPage: keyof typeof pages;
+    if (location.pathname === "/") {
+        if (location.hash.startsWith("#") && location.hash.length === 8) {
+            nextPage = "download";
+        } else {
+            location.hash = "";
+            nextPage = "index";
+        }
+    } else {
+        location.replace("/");
+        nextPage = "nothing";
+    }
+    if (currentPageName !== nextPage) {
+        currentElement && currentElement.$destroy();
+        currentElement = pages[nextPage]();
+        currentPageName = nextPage;
+    }
 }
 
-export {};
+window.addEventListener("hashchange", render);
+render();
