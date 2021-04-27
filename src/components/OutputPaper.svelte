3c02d0de472b8223938df6e36216141c6e536180
Heavenstone <malolegendrelemaire@gmail.com>
2021-04-27 21:38:52 +0200

    reformatted OutputPaper

--- src/components/OutputPaper.svelte
+++ src/components/OutputPaper.svelte
@@ -3,23 +3,22 @@
 </script>
 
 <div
-    class="absolute inset-x-12 bottom-0 h-11 transform translate-y-full overflow-hidden {text ==
-    null
-        ? 'invisible'
-        : ''}"
+    class="absolute inset-x-12 bottom-0 h-11
+    transform translate-y-full overflow-hidden
+    {text == null
+    ? 'invisible'
+    : ''}
+    "
 >
     <div
-        class="
-                absolute inset-0 rounded-b transform-gpu bg-white
-                transition-transform duration-200 ease-in-out text-black
-                flex justify-center items-center
-                {text ==
-        null
-            ? '-translate-y-full'
-            : ''}
-                "
+        class="absolute inset-0 rounded-b transform-gpu bg-white
+        transition-transform duration-200 ease-in-out text-black
+        flex justify-center items-center
+        {text ==null
+        ? '-translate-y-full'
+        : ''}
+        "
     >
         {text}
-        <!--        {window.location.protocol}//{window.location.host}/#{finalCode}-->
     </div>
 </div>
